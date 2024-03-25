35e43265748a2699ae1b76a2a6c34c602be62555
Marchabar <marchabar1@alum.us.es>
2024-03-25 18:27:12 +0100

    refactor: change routes

--- routes/EnergyRoutes.mjs
+++ routes/EnergyRoutes.mjs
@@ -13,8 +13,8 @@ const router = Router();
 router.get("/energies", getEnergies);
 router.get("/energies/:id", getEnergy);
 router.delete("/energies", deleteAllEnergies);
-router.delete("/energies/delete/:id", deleteEnergy);
-router.post("/energies/create", createEnergy);
-router.put("/energies/update/:id", updateEnergy);
+router.delete("/energies/:id", deleteEnergy);
+router.post("/energies/", createEnergy);
+router.put("/energies/:id", updateEnergy);
 
 export default router;
