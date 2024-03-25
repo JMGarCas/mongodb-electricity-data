cdcd19ad1510594a18a5a0289fb762fa9bf355bb
Marchabar <marchabar1@alum.us.es>
2024-03-25 18:20:34 +0100

    fix: add comma values and lower case

--- controllers/PopulateDatabaseController.mjs
+++ controllers/PopulateDatabaseController.mjs
@@ -7,34 +7,39 @@ const router = Router();
 
 export default router.get("/populate-database", async (req, res) => {
   let energies = await Energy.find({});
+
   if (energies.length > 0) {
     return res.status(400).send("Database already populated");
   }
   fs.createReadStream("./static/global-data-on-sustainable-energy.csv")
-    .pipe(csv())
+    .pipe(csv({
+      mapHeaders: ({ header }) => header.trim().replace(/[^a-z0-9]/gi, '_').toLowerCase(),
+      mapValues: ({ value }) => value.replace(/,/g, '.')
+  }))
     .on("data", async (row) => {
+      console.log(row);
       const energy = new Energy({
-        Entity: row.Entity,
-        Year: row.Year,
-        Access_to_electricity_percent_of_population: row.Access_to_electricity_percent_of_population,
-        Access_to_clean_fuels_for_cooking: row.Access_to_clean_fuels_for_cooking,
-        Renewable_electricity_generating_capacity_per_capita: row.Renewable_electricity_generating_capacity_per_capita,
-        Financial_flows_to_developing_countries_US_dollars: row.Financial_flows_to_developing_countries_US_dollars,
-        Renewable_energy_share_in_the_total_final_energy_consumption_percent: row.Renewable_energy_share_in_the_total_final_energy_consumption_percent,
-        Electricity_from_fossil_fuels_TWh: row.Electricity_from_fossil_fuels_TWh,
-        Electricity_from_nuclear_TWh: row.Electricity_from_nuclear_TWh,
-        Electricity_from_renewables_TWh: row.Electricity_from_renewables_TWh,
-        Low_carbon_electricity_percent_electricity: row.Low_carbon_electricity_percent_electricity,
-        Primary_energy_consumption_per_capita_kWh_person: row.Primary_energy_consumption_per_capita_kWh_person,
-        Energy_intensity_level_of_primary_energy_MJ_2017_PPP_GDP: row.Energy_intensity_level_of_primary_energy_MJ_2017_PPP_GDP,
-        Value_co2_emissions_kt_by_country: row.Value_co2_emissions_kt_by_country,
-        Renewables_percent_equivalent_primary_energy: row.Renewables_percent_equivalent_primary_energy,
+        entity: row.entity,
+        year: row.year,
+        access_to_electricity_percent_of_population: row.access_to_electricity____of_population_,
+        access_to_clean_fuels_for_cooking: row.access_to_clean_fuels_for_cooking,
+        renewable_electricity_generating_capacity_per_capita: row.renewable_electricity_generating_capacity_per_capita,
+        financial_flows_to_developing_countries_us_dollars: row.financial_flows_to_developing_countries__us___,
+        renewable_energy_share_in_the_total_final_energy_consumption_percent: row.renewable_energy_share_in_the_total_final_energy_consumption____,
+        electricity_from_fossil_fuels_twh: row.electricity_from_fossil_fuels__twh_,
+        electricity_from_nuclear_twh: row.electricity_from_nuclear__twh_,
+        electricity_from_renewables_twh: row.electricity_from_renewables__twh_,
+        low_carbon_electricity_percent_electricity: row.low_carbon_electricity____electricity_,
+        primary_energy_consumption_per_capita_kwh_person: row.primary_energy_consumption_per_capita__kwh_person_,
+        energy_intensity_level_of_primary_energy_mj_2017_ppp_gdp: row.energy_intensity_level_of_primary_energy__mj__2017_ppp_gdp_,
+        value_co2_emissions_kt_by_country: row.value_co2_emissions_kt_by_country,
+        renewables_percent_equivalent_primary_energy: row.renewables____equivalent_primary_energy_,
         gdp_growth: row.gdp_growth,
         gdp_per_capita: row.gdp_per_capita,
-        Density_P_Km2: row.Density_P_Km2,
-        Land_Area_Km2: row.Land_Area_Km2,
-        Latitude: row.Latitude,
-        Longitude: row.Longitude,
+        density_p_km2: row.density_n_p_km2_,
+        land_area_km2: row.land_area_km2_,
+        latitude: row.latitude,
+        longitude: row.longitude,
       });
       await energy.save();
     })
